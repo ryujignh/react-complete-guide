0ae3f76675a9c80e082eaabb95bdb25524bbef7c
ryujignh <ryujignh@gmail.com>
2020-03-11 07:35:53 +0900

    Removing StyledComponents for another way to handle CSS

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
 import React, {Component, useState} from 'react';
-import styled from "styled-components";
 import './App.css';
 import Person from './Person/Person'
-
-const StyledButton = styled.button`
-    background-color: ${props => props.alt ? 'red' : 'green'};
-    color: white;
-    font: inherit;
-    border: 1px solid blue;
-    padding: 8px;
-    cursor: pointer;
-    
-    &:hover {
-        background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
-        color: black;
-    }
-`;
 
 class App extends Component {
     state = {
@@ -114,7 +99,7 @@ class App extends Component {
                 <h1>Hi, I'm a React App</h1>
                 <p className={classes.join(' ')}>this is really sowking</p>
                 <p>{this.state.otherState}</p>
-                <StyledButton alt={this.state.showPersons} onClick={this.togglePersonsHandler}>Toggle persons</StyledButton>
+                <button className='button' onClick={this.togglePersonsHandler}>Toggle persons</button>
 
                 {persons}
 
