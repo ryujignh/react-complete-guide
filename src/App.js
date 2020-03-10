8d8c2b02caff704ce6f82e66b2016513f01597af
ryujignh <ryujignh@gmail.com>
2020-03-10 14:11:22 +0900

    Using Radium to use hover

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component, useState} from 'react';
 import './App.css';
+import Radium from "radium";
 import Person from './Person/Person'
 
 class App extends Component {
@@ -56,6 +57,10 @@ class App extends Component {
             border: '1px solid blue',
             padding: '8px',
             cursor: 'pointer',
+            ':hover': {
+                backgroundColor: 'lightgreen',
+                color: 'black',
+            }
         };
 
         let persons = null;
@@ -74,6 +79,10 @@ class App extends Component {
                 </div>
             );
             style.backgroundColor = 'red';
+            style[':hover'] = {
+                backgroundColor: 'salmon',
+                color: 'black',
+            };
         }
 
         const classes = [];
@@ -103,46 +112,4 @@ class App extends Component {
     }
 }
 
-export default App;
-
-
-// const App = props => {
-//     const [personsState, setPersonsState] = useState({
-//         persons: [
-//             {name: 'Max', age: 28},
-//             {name: 'Maxaaa', age: 29},
-//             {name: 'Stept', age: 22},
-//         ],
-//         otherState: 'Hello world'
-//
-//     });
-//
-//     const switchNameHundler = () => {
-//         setPersonsState({
-//             persons: [
-//                 {name: 'Ryuji', age: 31},
-//                 {name: 'Maxaaa', age: 29},
-//                 {name: 'Stept', age: 22},
-//             ]
-//         })
-//     }
-//
-//     return (
-//         <div className="App">
-//             <h1>Hi, I'm a React App</h1>
-//             <p>this is really sowking</p>
-//             <p>{personsState.otherState}</p>
-//             <button onClick={switchNameHundler}>Switch Name</button>
-//             <Person
-//                 name={personsState.persons[0].name}
-//                 age={personsState.persons[0].age}/>
-//             <Person
-//                 name={personsState.persons[1].name}
-//                 age={personsState.persons[1].age}
-//                 click={this.switchNameHandler}/>
-//             <Person
-//                 name={personsState.persons[2].name}
-//                 age={personsState.persons[2].age}/>
-//         </div>
-//     );
-// }
+export default Radium(App);
