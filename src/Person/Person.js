a2b6dda5e59a71af693eeb893a62609d655337b3
ryujignh <ryujignh@gmail.com>
2020-03-11 08:05:05 +0900

    Using CSS modules in Person.js

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,14 +1,9 @@
 import React from 'react';
-// import './Person.css';
+import classes from './Person.css';
 
 const person = (props) => {
-    const style = {
-        '@media (min-width: 500px)': {
-            width: '450px'
-        }
-    }
     return (
-        <div>
+        <div className={classes.Person}>
             <p onClick={props.click}> I 'm a {props.name} and I am {props.age} years old!</p>
             <p> {props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
